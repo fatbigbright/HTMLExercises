4f28fc9a7a5196eaa6197610916553badd63515e
Chang,Liang <bigbrightf@gmail.com>
2013-12-16 12:37:56 +0800

    strange, previous update did not uploaded. also fix a little text in html file.

--- public/scripts/game-of-life.js
+++ public/scripts/game-of-life.js
@@ -1,101 +1,135 @@
 //$(document).ready(function(){
 window.onload = function(){
     //var canvas = $('#myCanvas')[0];
-    var canvas = document.getElementById('myCanvas');
-    var context = canvas.getContext('2d');
-
-    var BLOCK_WIDTH = 5;
-    var BLOCK_HEIGHT = 5;
+    var getCanvasContext = function(id){
+        var canvas = document.getElementById(id);
+        var context = canvas.getContext('2d');
+        return context;
+    };
+    var context = getCanvasContext('myCanvas');
 
     var WIDTH = 540;
     var HEIGHT = 750;
 
-    var MAX_AXIS_X = 108;
-    var MAX_AXIS_Y = 150;
-
-    var game = {
-        currentFrame: [],
-        nextFrame: [],
-        aliveColor: "#125643",
-        deadColor: "#bebebe",
-        initWithArray: function(array){
-            this.swap(this.currentFrame, array);
-            this.swap(this.nextFrame, this.currentFrame);
-        },
-        init: function(){
-            this.currentFrame = new Array();
-            for(var x = 0; x < MAX_AXIS_X ; x++){
-                this.currentFrame[x] = new Array();
-                for(var y = 0; y < MAX_AXIS_Y; y++){
-                    this.currentFrame[x][y] = Math.floor(Math.random()*20)%20 == 1 ? 1 : 0;
+    var Game = function(){
+        var game = {
+            canvasContext: null,
+            blockWidth: 5,
+            blockHeight: 5,
+            maxAxisX: 108,
+            maxAxisY: 150,
+            currentFrame: [],
+            nextFrame: [],
+            aliveColor: "#125643",
+            deadColor: "#bebebe",
+            initForTest: function(array,canvasContext,blockWidth,blockHeight,maxAxisX,maxAxisY){
+                this.canvasContext = canvasContext;
+                this.blockWidth = blockWidth;
+                this.blockHeight = blockHeight;
+                this.maxAxisX = maxAxisX;
+                this.maxAxisY = maxAxisY;
+                this.swap(this.currentFrame, array);
+                this.swap(this.nextFrame, this.currentFrame);
+            },
+            init: function(canvasContext){
+                this.canvasContext = canvasContext;
+                this.currentFrame = new Array();
+                for(var x = 0; x < this.maxAxisX ; x++){
+                    this.currentFrame[x] = new Array();
+                    for(var y = 0; y < this.maxAxisY; y++){
+                        this.currentFrame[x][y] = Math.floor(Math.random()*2)%2 == 1 ? 1 : 0;
+                    }
                 }
-            }
-            this.swap(this.nextFrame, this.currentFrame);
-        },
-        draw: function(){
-            for(var x = 0; x < MAX_AXIS_X ; x++){
-                for(var y = 0; y < MAX_AXIS_Y; y++){
-                    this.fillBlock(x, y, this.currentFrame[x][y]);
+                this.swap(this.nextFrame, this.currentFrame);
+            },
+            draw: function(){
+                for(var x = 0; x < this.maxAxisX ; x++){
+                    for(var y = 0; y < this.maxAxisY; y++){
+                        this.fillBlock(x, y, this.currentFrame[x][y]);
+                    }
                 }
-            }
-        },
-        fillBlock: function(x, y, alive){
-            if(x >= MAX_AXIS_X || y >= MAX_AXIS_Y) return;
-            var realX = y * BLOCK_WIDTH;
-            var realY = x * BLOCK_HEIGHT;
+            },
+            fillBlock: function(x, y, alive){
+                if(x >= this.maxAxisX || y >= this.maxAxisY) return;
+                var realX = y * this.blockWidth;
+                var realY = x * this.blockHeight;
 
-            context.fillStyle = alive ? this.aliveColor : this.deadColor;
-            context.fillRect(realX, realY, BLOCK_WIDTH, BLOCK_HEIGHT);
-        },
-        swap: function(destination, source){
-            for(var index = 0; index < MAX_AXIS_X; index++){
-                destination[index] = source[index].slice();
-            }
-        },
-        nextTick: function(){
-            var isAroundCellAvailable = function(centerX, centerY, x, y){
-                return !((x == centerX && y == centerY) || x < 0 || x >= MAX_AXIS_X || y < 0 || y >= MAX_AXIS_Y);
-            };
-            var getAroundAlive = function(frame, x, y){
+                this.canvasContext.fillStyle = alive ? this.aliveColor : this.deadColor;
+                this.canvasContext.fillRect(realX, realY, this.blockWidth, this.blockHeight);
+            },
+            swap: function(destination, source){
+                for(var index = 0; index < this.maxAxisX; index++){
+                    destination[index] = source[index].slice();
+                }
+            },
+            isAroundCellAvailable: function(centerX, centerY, x, y){
+                return !((x == centerX && y == centerY) || x < 0 || x >= this.maxAxisX || y < 0 || y >= this.maxAxisY);
+            },
+            getAroundAlive: function(x, y){
                 var aroundAlive = 0;
                 for(var i = x - 1; i <= x + 1; i++){
                     for(var j = y - 1; j <= y + 1; j++){
-                        if(!isAroundCellAvailable(x, y, i, j)) 
+                        if(!this.isAroundCellAvailable(x, y, i, j)) 
                             continue;
-                        if(frame[i][j] == 1)
+                        if(this.currentFrame[i][j] == 1)
                             aroundAlive++;
                     }
                 }
                 return aroundAlive;
-            };
-            for(var x = 0; x < MAX_AXIS_X ; x++){
-                for(var y = 0; y < MAX_AXIS_Y; y++){
-                    var aroundAlive = getAroundAlive(this.currentFrame, x, y);
+            },
+            nextTick: function(){
+                for(var x = 0; x < this.maxAxisX ; x++){
+                    for(var y = 0; y < this.maxAxisY; y++){
+                        var aroundAlive = this.getAroundAlive(x, y);
 
-                    if(this.currentFrame[x][y] == 1){
-                        if(aroundAlive < 2 || aroundAlive > 3)
-                            this.nextFrame[x][y] = 0;
-                    }
-                    else{
-                        if(aroundAlive >= 3)
-                            this.nextFrame[x][y] = 1;
+                        if(this.currentFrame[x][y] == 1){
+                            if(aroundAlive < 2 || aroundAlive > 3)
+                                this.nextFrame[x][y] = 0;
+                        }
+                        else{
+                            if(aroundAlive == 3)
+                                this.nextFrame[x][y] = 1;
+                        }
                     }
                 }
+                
+                // Copy nextFrame to currentFrame at last
+                // 2-dimention array is not the same as 1-dimention!!!!
+                //this.currentFrame = this.nextFrame.slice();
+                this.swap(this.currentFrame, this.nextFrame);
             }
-            
-            // Copy nextFrame to currentFrame at last
-            // 2-dimention array is not the same as 1-dimention!!!!
-            //this.currentFrame = this.nextFrame.slice();
-            this.swap(this.currentFrame, this.nextFrame);
-        }
+        };
+        return game;
     };
-    game.init();
-    //game.initWithArray([[0, 0, 0], [1, 1, 1], [0, 0, 0]]);
+
+    var game = new Game();
+    game.init(context);
 
     var axis_x = 0;
     var axis_y = 0;
     setInterval(function(){
         game.draw();
         game.nextTick();
-    }, 1000/15);
+    }, 1000/10);
+
+    //initForTest: function(array,canvasContext,blockWidth,blockHeight,maxAxisX,maxAxisY)
+    var RunTest = function(canvasID, array, blockWidth, blockHeight, maxAxisX, maxAxisY){
+        var canvas = document.getElementById(canvasID);
+        var context = canvas.getContext('2d');
+
+        var testGame = new Game();
+        testGame.initForTest(array, context, blockWidth, blockHeight, maxAxisX, maxAxisY);
+        setInterval(function(){
+            testGame.draw();
+            testGame.nextTick();
+        }, 1000/1);
+    };
+    RunTest('test1', [[0,0,0],[1,1,1],[0,0,0]], 30, 30, 3, 3);
+    RunTest('test2', [[1,1,0],[0,1,0],[0,1,0]], 30, 30, 3, 3);
+    RunTest('test3', [[0,1,0],[1,1,1],[0,1,0]], 30, 30, 3, 3);
+    RunTest('test4', [[1,1,0],[0,1,0],[0,1,1]], 30, 30, 3, 3);
+    RunTest('test5', [[0,1,1,0],[0,1,0,1],[0,0,1,1],[1,0,1,1]], 30, 30, 4, 4);
+    RunTest('test6', [[0,1,1,0],[1,1,1,1],[1,1,1,1],[0,1,1,0]], 30, 30, 4, 4);
+    RunTest('test7', [[1,1,1,1],[1,0,0,1],[1,0,0,1],[1,1,1,1]], 30, 30, 4, 4);
+    RunTest('test8', [[1,0,0,1],[0,1,1,0],[0,1,1,0],[1,0,0,1]], 30, 30, 4, 4);
 };
